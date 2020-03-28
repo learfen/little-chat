bcb132504f0fc644b0fe5bf3f7ab11cffa7bf633
learfen <learfen001@gmail.com>
2020-03-28 17:00:42 -0300

    sequelize script conection

--- app/database/sequelize.js
+++ app/database/sequelize.js
@@ -1,28 +1,31 @@
+    
+module.exports = (models) => {
+    const Sequelize = require('sequelize')
+    let modelsResponse = {}
+    for(let model of models){
+        modelsResponse[model] = require(`./models/${model}Instance`).schema()
+    }
 
-const Sequelize = require('sequelize')
-let modelsResponse = {}
-for(let model of models){
-    modelsResponse[model] = require('./models/'+model).schema()
+    var sequelize = new Sequelize('database', 'root', '', {
+        host: 'localhost',
+        dialect: 'sqlite',
+    
+        pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+        },
+    
+        // SQLite only
+        storage: './database.sqlite'
+    });
+
+    for(let modelOverride in modelsResponse){
+        modelsResponseOverride[modelOverride] = modelResponse[modelOverride](sequelize, Sequelize)
+    }
+
+    sequelize.sync({ force: true }).then(() => {
+        console.log(`DB and tables found`)
+    })
+    return  modelsResponseOverride
 }
-
-var sequelize = new Sequelize('database', 'root', '', {
-    host: 'localhost',
-    dialect: 'sqlite',
-  
-    pool: {
-      max: 5,
-      min: 0,
-      idle: 10000
-    },
-  
-    // SQLite only
-    storage: './database.sqlite'
-});
-
-const User = UserModel(sequelize, Sequelize)
-
-sequelize.sync({ force: true }).then(() => {
-    console.log(`Database & tables created!`)
-})
-
-module.exports = modelsResponse
